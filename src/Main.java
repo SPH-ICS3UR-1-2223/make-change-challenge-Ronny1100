303e2afc98fd8e6ea8c42108d50660c5ed5d236b
Ronny1100 <113380723+Ronny1100@users.noreply.github.com>
2022-10-18 14:40:52 -0400

    main

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,12 @@ public class Main {
 		//$100,$50,$20,$10,$5,$2,$1,$0.25,$0.10,$0.05
 		//As a bonus, make your project round to the nearest $0.05 like actual stores.
 		
-	}
-
-}
+			System.out.println("Total amount is $13.85");
+			System.out.println("Amount Paid is $50.00");
+			if (amountPaid - totalAmount == returnChange);
+			else if (50.00 - 13.87 == 36.15);
+			System.out.println("Change is $36.15");
+		}
+		
+		
+		}	
