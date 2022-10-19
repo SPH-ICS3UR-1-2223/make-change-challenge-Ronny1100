e59586b72e5b21baf5ffb1fe3883ce7e0d757d12
Ronny1100 <113380723+Ronny1100@users.noreply.github.com>
2022-10-19 14:38:35 -0400

    updated

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+	
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 		//Your challenge is to read in a price
@@ -10,13 +11,30 @@ public class Main {
 		//Break the change into all denominations: 
 		//$100,$50,$20,$10,$5,$2,$1,$0.25,$0.10,$0.05
 		//As a bonus, make your project round to the nearest $0.05 like actual stores.
+			double TotalAmount = 13.85; {
+			double AmountPaid = 50.00;
+			String Change = "100; 50; 20; 10; 5; 2; 1; 0.25; 0.10; 0.05"; {
+			System.out.println("Enter a payment");
+			double payment = in.nextDouble ();
+			System.out.println("You paid &"+Math.round(payment*100)/100.0);
+			System.out.println("Your change is");
+		double change = 123.44;
+		int lastDigit = (int)((change*100)%10); {
+		if (lastDigit ==1 || lastDigit ==2) {
+			change = change-(lastDigit/100.0);
+		}else if (lastDigit==3 || lastDigit==4) {
+			change = change + ((5-lastDigit)/100.0);
+		}
+		System.out.println(change); {
+			
+			}
 		
-			System.out.println("Total amount is $13.85");
-			System.out.println("Amount Paid is $50.00");
-			if (amountPaid - totalAmount == returnChange);
-			else if (50.00 - 13.87 == 36.15);
-			System.out.println("Change is $36.15");
 		}
 		
-		
-		}	
+	}
+			
+	}
+	}
+	
+}
+			
